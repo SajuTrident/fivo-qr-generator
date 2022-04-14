0f8ff9d11b3925a606bad5a9f7fb622fb0eae62f
Sajad pp <96676224+SajuTrident@users.noreply.github.com>
2022-04-14 21:40:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function home(){
+  location.replace("./index.html");
+}
+function youtube(){
+  location.replace('https://youtube.com/c/SajuTalk');
+}
+function github(){
+  location.replace('https://github.com/SajuTrident');
+}
+function discord(){
+  location.replace('https://discord.gg/uMCkw7fZht');
+}
+function reddit(){
+  location.replace('https://www.reddit.com/u/Crazycln?utm_medium=android_app&utm_source=share');
+}
+
+/*const inputText = document.querySelector("#inp");
+const imgArea = document.querySelector(".qr-d");
+const btn = document.querySelector("#sbt");
+const qrImg = document.querySelector(".qrImg");*/
+//const dwC = document.querySelector(".dw-c");
+//const dwBtn = document.querySelector(".dw-btn");
+/*function getInputValue(e){
+  inputText = e.target.value
+  if(inputText.length <=0 || inputText === ""){
+    imgArea.classList.remove("active");
+    qrImg.removeAttribute("src");*/
+    /*dWc.classList.remove("active");
+    dwBtn.removeAttribute("src");*/
+ /* }
+}
+btn.addEventListener("click", function(){
+  if(inputText.length > 0){
+    imgArea.classList.add("active");*/
+    //dwC.classList.add("active");
+    //qrImg.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputText}'
+    /*function downLoad(){
+      location.replace('https://api.qr-code-generator.com/v1/?access-token=your-acces-token-here${inputT   ext}');
+    }*/
+ /* }
+})*/
